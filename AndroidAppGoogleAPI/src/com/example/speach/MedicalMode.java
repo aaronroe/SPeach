3959a99cae4f5c34d5222d7d4fdb509cb5760ffb
rf7 <rf7@rice.edu>
2014-01-27 11:47:31 -0600

    Added GoogleAPI Speech recognizer

--- AndroidAppGoogleAPI/src/com/example/speach/MedicalMode.java
+++ AndroidAppGoogleAPI/src/com/example/speach/MedicalMode.java
@@ -1,8 +1,15 @@
 package com.example.speach;
 
+import java.util.ArrayList;
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MedicalMode extends Activity {
 
@@ -18,5 +25,41 @@ public class MedicalMode extends Activity {
 		getMenuInflater().inflate(R.menu.medical_mode, menu);
 		return true;
 	}
+	
+	public void clearText(View view) {
+		EditText displayText = (EditText) findViewById(R.id.display_message);
+        displayText.setText("");
+	}
+	
+	public void startRecognizer(View view) {
+    	Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+        try {
+            startActivityForResult(intent, 1);
+        } catch (ActivityNotFoundException a) {
+            Toast t = Toast.makeText(getApplicationContext(),
+                    "Oops! Your device doesn't support Speech to Text",
+                    Toast.LENGTH_SHORT);
+            t.show();
+        }
+    }
+	
+	@Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+ 
+        switch (requestCode) {
+        case 1: {
+            if (resultCode == RESULT_OK && null != data) {
+
+                ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                EditText displayText = (EditText) findViewById(R.id.display_message);
+                displayText.append(text.get(0)+"\n");
+            }
+            break;
+        }
+ 
+        }
+    }
 
 }
