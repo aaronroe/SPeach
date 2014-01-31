2bc36c2a10774ea65a3d52593e1e22d781b8c035
rf7 <rf7@rice.edu>
2014-01-31 11:39:08 -0600

    commented

--- AndroidAppGoogleAPI/src/com/example/speach/MainMenu.java
+++ AndroidAppGoogleAPI/src/com/example/speach/MainMenu.java
@@ -6,14 +6,22 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 
+/**
+ * This is the first activity that loads on boot.
+ * It has three big buttons: Medical Mode, Training Mode, Combined Mode
+ * @author Riya
+ *
+ */
 public class MainMenu extends Activity {
-
+	
+	/** AUTO GENERATED **/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_menu);
     }
-
+    
+    /** AUTO GENERATED **/
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -21,6 +29,10 @@ public class MainMenu extends Activity {
         return true;
     }
     
+    /**
+     * Starts the Medical Mode activity
+     * @param view
+     */
     public void startMedicalMode(View view) {
     	Intent intent = new Intent(this, MedicalMode.class);
     	startActivity(intent);
--- AndroidAppGoogleAPI/src/com/example/speach/MedicalMode.java
+++ AndroidAppGoogleAPI/src/com/example/speach/MedicalMode.java
@@ -14,10 +14,32 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+/**
+ * The medical mode.
+ * It has three button: Start, Stop and Clear
+ * Start will start the speech-to-text conversion, until the Stop button is pressed.
+ * Clear button will clear out all the text in the textbox
+ * @author Riya
+ *
+ */
 public class MedicalMode extends Activity {
+	/**
+	 * Android Speech Recognizer
+	 */
 	private SpeechRecognizer sr;
+	
+	/**
+	 * The intent that is passed along throughout the life of the activity
+	 */
 	private Intent intent;
 	
+	/**
+	 * A inner class implements the RecognitionListener so that 
+	 * we can use the speech API without the annoying google dialogue.
+	 * 
+	 * @author Riya
+	 *
+	 */
 	private class listener implements RecognitionListener {
 
 		@Override
@@ -92,13 +114,18 @@ public class MedicalMode extends Activity {
 			//System.out.println("onReadyForSpeech");
 		}
 
+		/**
+		 * Recursively calls startListening() so that the speech recognizer will keep on listening to the voice
+		 * Note that during the time that the results are made and startListening() called, all utterances are 
+		 * not buffered and therefore lost
+		 */
 		@Override
 		public void onResults(Bundle data) {
 			//System.out.println("onResults");
-			ArrayList<String> text = data.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+			ArrayList<String> text = data.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION); // obtain the result
 			EditText displayText = (EditText) findViewById(R.id.display_message);
-            displayText.append(text.get(0)+"\n");
-            sr.startListening(intent);
+            displayText.append(text.get(0)+"\n"); // put text on the text box
+            sr.startListening(intent); // recursively call
 		}
 
 		@Override
@@ -107,15 +134,21 @@ public class MedicalMode extends Activity {
 		}
 		
 	}
-
+	
+	/** AUTO GENERATED **/
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_medical_mode);
-		sr = SpeechRecognizer.createSpeechRecognizer(this);       
-        sr.setRecognitionListener(new listener());
+		
+		/**
+		 * Speech Recognition
+		 */
+		sr = SpeechRecognizer.createSpeechRecognizer(this); 
+        sr.setRecognitionListener(new listener()); // Sets the RecognizerListner to the inner class
 	}
-
+	
+	/** AUTO GENERATED **/
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
@@ -123,53 +156,33 @@ public class MedicalMode extends Activity {
 		return true;
 	}
 	
+	/**
+	 * Clears the text written in the text box
+	 * @param view
+	 */
 	public void clearText(View view) {
 		EditText displayText = (EditText) findViewById(R.id.display_message);
         displayText.setText("");
 	}
 	
+	/**
+	 * Stops the Recognizer
+	 * @param view
+	 */
 	public void stopRecognizer(View view) {
 		sr.stopListening();
 		sr.cancel();
 	}
 	
+	/**
+	 * Starts the Recognizer
+	 * @param view
+	 */
 	public void startRecognizer(View view) {
 		intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName());
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US"); // TODO: Try out the "FREE_FORM"
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, this.getPackageName()); // Need to specify this
         sr.startListening(intent);
     }
-	
-//	private void startRecognizerHelper() {
-//		Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
-//        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
-//        try {
-//            startActivityForResult(intent, 1);
-//        } catch (ActivityNotFoundException a) {
-//            Toast t = Toast.makeText(getApplicationContext(),
-//                    "Oopps! Your device doesn't support Speech to Text",
-//                    Toast.LENGTH_SHORT);
-//            t.show();
-//        }
-//	}
-//	
-//	@Override
-//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-// 
-//        switch (requestCode) {
-//        case 1: {
-//            if (resultCode == RESULT_OK && null != data) {
-//
-//                ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-//                EditText displayText = (EditText) findViewById(R.id.display_message);
-//                displayText.append(text.get(0)+"\n");
-//                startRecognizerHelper();
-//            }
-//            break;
-//        }
-// 
-//        }
-//    }
 
 }
